8e016681ec616c091e8a7a2adb5e3c97dd4fe723
Renat Tuktarov <yandzeek@gmail.com>
2025-02-24 21:09:11 +0400

    dropped redundant transformers

--- pkg/str/transform/transform.go
+++ pkg/str/transform/transform.go
@@ -11,8 +11,6 @@ var (
 	ErrChain     = errors.New("Failed to chain transformers")
 
 	ErrCast = errors.New("Failed to cast value type")
-	// ErrNotString = errors.New("Failed to convert value to string")
-	// ErrNotSlice  = errors.New("Failed to convert value to string slice")
 )
 
 type State struct {
@@ -27,17 +25,8 @@ type Transformer interface {
 type StateFn = func(*State, common.KVOptions) error
 type MapFromToFn[F, T any] = func(F, common.KVOptions) (T, error)
 
-type MapFn = MapFromToFn[string, string]
-type MapSliceFn = MapFromToFn[[]string, []string]
-type MapIntoFn[T any] = MapFromToFn[string, T]
-type SplitFn = MapFromToFn[string, []string]
-type JoinFn = MapFromToFn[[]string, string]
-
-// func (s *State) GetStringValue() (string, error) {
-// 	str, ok := s.Value.(string)
-// 	if !ok {
-// 		return "", ErrNotString
-// 	}
-//
-// 	return str, nil
-// }
+// type MapFn = MapFromToFn[string, string]
+// type MapSliceFn = MapFromToFn[[]string, []string]
+// type MapIntoFn[T any] = MapFromToFn[string, T]
+// type SplitFn = MapFromToFn[string, []string]
+// type JoinFn = MapFromToFn[[]string, string]
--- pkg/str/transform/transformers.go
+++ pkg/str/transform/transformers.go
@@ -7,41 +7,11 @@ import (
 	"github.com/yandzee/config/pkg/str/parse"
 )
 
-func Map(fn MapFn) Transformer {
-	return MapFromTo(func(s string, opts common.KVOptions) (string, error) {
-		return fn(s, opts)
-	})
-}
-
-func MapSlice(fn MapSliceFn) Transformer {
-	return MapFromTo(func(s []string, opts common.KVOptions) ([]string, error) {
-		return fn(s, opts)
-	})
-}
-
-func MapInto[T any](fn MapIntoFn[T]) Transformer {
-	return MapFromTo(func(s string, opts common.KVOptions) (T, error) {
-		return fn(s, opts)
-	})
-}
-
-func Split(fn SplitFn) Transformer {
-	return MapFromTo(func(s string, opts common.KVOptions) ([]string, error) {
-		return fn(s, opts)
-	})
-}
-
-func Join(fn JoinFn) Transformer {
-	return MapFromTo(func(s []string, opts common.KVOptions) (string, error) {
-		return fn(s, opts)
-	})
-}
-
 func Parse[T any](fn parse.Fn[T]) Transformer {
-	return MapInto(fn)
+	return Map(fn)
 }
 
-func MapFromTo[F, T any](fn MapFromToFn[F, T]) Transformer {
+func Map[F, T any](fn MapFromToFn[F, T]) Transformer {
 	return StateTransform(func(s *State, opts common.KVOptions) error {
 		val, ok := s.Value.(F)
 		fmt.Printf("StateTransform: cast to From type: %v (%T) %v\n", val, val, ok)
