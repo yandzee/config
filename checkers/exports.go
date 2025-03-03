a3ab0c0d084dc47401fa39128d650b8458e4efaf
Renat Tuktarov <yandzeek@gmail.com>
2025-03-03 17:50:29 +0400

    additional str checks

--- checkers/exports.go
+++ checkers/exports.go
@@ -1,6 +1,20 @@
 package checkers
 
-import "github.com/yandzee/config/check"
+import (
+	"fmt"
+
+	"github.com/yandzee/config/check"
+)
+
+var (
+	StrNotEmpty = Fn(func(str string) (bool, string) {
+		if len(str) == 0 {
+			return false, "String is empty"
+		}
+
+		return true, ""
+	})
+)
 
 func IsPositive[T RealNum]() check.Checker[T] {
 	v := float64(0)
@@ -22,4 +36,19 @@ func IsNegative[T RealNum]() check.Checker[T] {
 			RightIncluded: false,
 		},
 	}
+}
+
+func StrLength(l int) check.Checker[string] {
+	return Fn(func(str string) (bool, string) {
+		if n := len(str); n != l {
+			return false, fmt.Sprintf(
+				"String `%s` has wrong length, expected %d, got: %d",
+				str,
+				l,
+				n,
+			)
+		}
+
+		return true, ""
+	})
 }
